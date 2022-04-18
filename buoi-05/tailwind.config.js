79f522735f1bb1ae9a08bb13d115876d3267ba13
Toàn <bnqtoan@gmail.com>
2022-04-18 20:58:35 +0700

    update buoi 5

---
+++ buoi-05/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "**/*.html"
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        gray: {
+          100: '#F4F5F7'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
